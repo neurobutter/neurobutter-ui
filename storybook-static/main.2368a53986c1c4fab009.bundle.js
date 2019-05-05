7629db7efcc7ec966d0db222a1eab30b78b59614
lx0n2acl <lx0n2acl@gmail.com>
2019-05-05 17:24:26 -0400

    added storybook static build and license

---
+++ storybook-static/main.2368a53986c1c4fab009.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{327:function(n,o,p){p(328),p(413),n.exports=p(662)},413:function(n,o,p){"use strict";p.r(o);p(414),p(659)}},[[327,1,2]]]);
---
+++ storybook-static/precache-manifest.113362790e17c9c89aaea9b2c193d6dd.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a033ad653f3a7f17c25349c53d316bf",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.309140337b65c76a8801.bundle.js"
+  },
+  {
+    "url": "runtime~main.309140337b65c76a8801.bundle.js"
+  },
+  {
+    "url": "vendors~main.309140337b65c76a8801.bundle.js"
+  }
+]);
